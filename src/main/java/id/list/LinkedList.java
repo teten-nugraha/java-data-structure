89f2f5ecefa74c758e22f89dd2e70f807642dcf3
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-11-16 08:33:34 +0700

    delete linked list

--- src/main/java/id/list/LinkedList.java
+++ src/main/java/id/list/LinkedList.java
@@ -45,6 +45,60 @@ public class LinkedList {
         }
     }
 
+    // delete a node
+    public static LinkedList deleteByKey(LinkedList list, int key) {
+        Node currNode = list.head;
+        Node prev = null;
+
+        // case 1.
+        // if head it self hold the key to be deleted
+        if(currNode != null && currNode.data == key) {
+            list.head = currNode.next;
+
+            System.out.println(key + " found and deleted");
+
+            return list;
+        }
+
+        //
+        // CASE 2:
+        // If the key is somewhere other than at head
+        //
+
+        // Search for the key to be deleted,
+        // keep track of the previous node
+        // as it is needed to change currNode.next
+        while(currNode != null && currNode.data != key) {
+            prev = currNode;
+            currNode = currNode.next;
+        }
+
+        // If the key was present, it should be at currNode
+        // Therefore the currNode shall not be null
+        if (currNode != null) {
+            // Since the key is at currNode
+            // Unlink currNode from linked list
+            prev.next = currNode.next;
+
+            // Display the message
+            System.out.println(key + " found and deleted");
+        }
+
+        //
+        // CASE 3: The key is not present
+        //
+
+        // If key was not present in linked list
+        // currNode should be null
+        if (currNode == null) {
+            // Display the message
+            System.out.println(key + " not found");
+        }
+
+        // return the List
+        return list;
+    }
+
     public static void main(String[] args) {
         /* Start with the empty list. */
         LinkedList list = new LinkedList();
@@ -65,5 +119,13 @@ public class LinkedList {
 
         // Print the LinkedList
         printList(list);
+
+        System.out.println();
+
+        // Delete node with value 1
+        // In this case the key is ***at head***
+        deleteByKey(list, 1);
+
+        deleteByKey(list, 5);
     }
 }
