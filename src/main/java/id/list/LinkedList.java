fdb50f139019bda2579dfd24c8c748be44edef2b
Teten Nugraha <teten.nugraha@allianz.co.id>
2020-11-16 08:24:28 +0700

    print linked list

--- src/main/java/id/list/LinkedList.java
+++ src/main/java/id/list/LinkedList.java
@@ -35,4 +35,35 @@ public class LinkedList {
         return list;
     }
 
+    // method to print linked list
+    public static void printList(LinkedList list) {
+        Node currNode = list.head;
+
+        while(currNode != null) {
+            System.out.print (currNode.data + "");
+            currNode = currNode.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        /* Start with the empty list. */
+        LinkedList list = new LinkedList();
+
+        //
+        // ******INSERTION******
+        //
+
+        // Insert the values
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+        list = insert(list, 6);
+        list = insert(list, 7);
+        list = insert(list, 8);
+
+        // Print the LinkedList
+        printList(list);
+    }
 }
